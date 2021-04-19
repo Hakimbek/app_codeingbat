369bbb31a2c32e217c2a407e6cb8cc359e6a4e21
Hakimbek <abduhakim.bahramov@gmail.com>
2021-04-19 19:35:11 +0300

    Added Table annotation with uniqueConstraints of name and language_id to Language class

--- src/main/java/uz/pdp/appcodingbat/entity/TaskCategory.java
+++ src/main/java/uz/pdp/appcodingbat/entity/TaskCategory.java
@@ -16,7 +16,7 @@ public class TaskCategory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     private String info;
